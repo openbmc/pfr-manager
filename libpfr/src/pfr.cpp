a9d68c03c79354ee34249868251c175102f93b24
Vikram Bodireddy <vikram.bodireddy@intel.com>
2019-11-12 16:26:29 +0530

    PFR: CPLD mailbox i2c address change to 0x38
    This commit change the PFR mailbox i2c address
    as the earlier address 0x70 had got conflicts with
    HSBP.
    
    Tested: tested on platform with debug CPLD.
    
    Change-Id: I99a2d89c7d558f5c7c2890151474afc647406aed
    Signed-off-by: Vikram Bodireddy <vikram.bodireddy@intel.com>

--- libpfr/src/pfr.cpp
+++ libpfr/src/pfr.cpp
@@ -29,7 +29,8 @@ namespace pfr
 // TODO: Dynamically pull these values from configuration
 // entity-manager, when needed
 static constexpr int i2cBusNumber = 4;
-static constexpr int i2cSlaveAddress = 0x70;
+// below given is 7bit address. Its 8bit addr is 0x70
+static constexpr int i2cSlaveAddress = 0x38;
 
 // CPLD mailbox registers
 static constexpr uint8_t cpldROTVersion = 0x01;
