e398b13a474cc32bb1ef952e10c83a938263fd2e
Chalapathi <chalapathix.venkataramashetty@intel.com>
2020-05-07 16:21:38 +0000

    fix for false PFR ProvisioningStatus in redfish
    start the PFR manager service when PFR supported on the platform and
    PFR-CPLD is available and PFR provisioned, Otherwilse, stop the PFR
    manager service.
    
    Tested:
    1. When PFR Supported, PFR-CPLD is available and PFR provisioned.
    Command: busctl tree xyz.openbmc_project.PFR.Manager
    Response:
    └─/xyz
      └─/xyz/openbmc_project
        ├─/xyz/openbmc_project/pfr
        └─/xyz/openbmc_project/software
          ├─/xyz/openbmc_project/software/bios_active
          ├─/xyz/openbmc_project/software/bios_recovery
          ├─/xyz/openbmc_project/software/bmc_active
          ├─/xyz/openbmc_project/software/bmc_recovery
          ├─/xyz/openbmc_project/software/cpld_active
          └─/xyz/openbmc_project/software/cpld_recovery
    
    2. When PFR not supported or PFR-CPLD is not available and PFR not
       provisioned.
    Command: busctl tree xyz.openbmc_project.PFR.Manager
    Response:
    Failed to introspect object / of service xyz.openbmc_project.PFR.Manager
    : The name is not activatable
    No objects discovered.
    
    Signed-off-by: Chalapathi <chalapathix.venkataramashetty@intel.com>
    Change-Id: I08fd29f52dfe078b5b67f07a2ebb53efd38dea0c

---
+++ pfr_manager_start.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+PFR_ID_REG=0x00
+PFR_PROV_REG=0x0a
+
+pfr_active_mode() {
+    address=$(busctl get-property xyz.openbmc_project.EntityManager $board/PFR xyz.openbmc_project.Configuration.PFR Address | cut -b 3-) || return 1
+    bus=$(busctl get-property xyz.openbmc_project.EntityManager $board/PFR xyz.openbmc_project.Configuration.PFR Bus | cut -b 3-) || return 1
+    #check for 0xde in register file 0
+    local id=$(i2cget -y $bus $address $PFR_ID_REG 2>/dev/null) || return 1
+    [ "$id" == "0xde" ] || return 1
+    local prov=$(i2cget -y $bus $address $PFR_PROV_REG 2>/dev/null) || return 1
+    prov=$((prov & 0x20))
+    [ "$prov" == "32" ] && return 0
+    return 1
+}
+
+#Read board name
+counter=0
+board=''
+
+while [ -z $board ]
+do
+   board=$(busctl call xyz.openbmc_project.ObjectMapper /xyz/openbmc_project/object_mapper xyz.openbmc_project.ObjectMapper  GetSubTreePaths sias "/xyz/openbmc_project/inventory" 0 2 "xyz.openbmc_project.Inventory.Item.Board" "xyz.openbmc_project.Inventory.Item.Chassis" | cut -b 7- | sed 's/.$//')
+   if [ $counter -eq 10 ]
+   then
+       echo "Unable to get board name"
+       exit
+   fi
+   counter=`expr $counter + 1`
+   sleep 5
+done
+
+#Read PFR property
+support=$(busctl get-property xyz.openbmc_project.EntityManager $board/PFR xyz.openbmc_project.Configuration.PFR PFRSupport | cut -b 4- | sed 's/.$//')
+
+echo "PFR Supported=$support"
+
+if [[ "$support" == 'true' ]] && pfr_active_mode
+then
+    echo "Starting PFR manager service"
+    /usr/bin/intel-pfr-manager
+    exit
+else
+    echo "Could not start PFR manager service"
+    exit
+fi
+
+
