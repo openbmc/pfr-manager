2c5d172ebc60a082795be4d817dddb003f56271c
Chalapathi Venkataramashetty <chalapathix.venkataramashetty@intel.com>
2021-05-17 04:27:21 +0000

    pfr-manager: update cpld error codes in new platform
    Add support to update the cpld error codes in new platform.
    
    Tested:
    Update BMC firmware with mismatched SVN.
    POST: https://<BMC_IP>/redfish/v1/UpdateService/
          with <BMC_Update_Capsule> binary file
    Update failed and event log received.
    Command: GET: https://<BMC_IP>/redfish/v1/Systems/system/LogServices/
                  EventLog/Entries
    Response:
     {
                "@odata.id": "/redfish/v1/Systems/system/LogServices/
                 EventLog/Entries/1621435142_1",
                "@odata.type": "#LogEntry.v1_4_0.LogEntry",
                "Created": "2021-05-19T14:39:02+00:00",
                "EntryType": "Event",
                "Id": "1621435142_1",
                "Message": "Firmware resiliency error. Error reason:
                 Firmware update failed(MinorCode:0x02).",
                "MessageArgs": [
                    "Firmware update failed(MinorCode:0x02)"
                ],
                "MessageId": "OpenBMC.0.1.FirmwareResiliencyError",
                "Name": "System Event Log Entry",
                "Severity": "Critical"
            },
    
    Signed-off-by: Chalapathi Venkataramashetty <chalapathix.venkataramashetty@intel.com>
    Change-Id: I406cb50695bd77b1beddb0788677af3657dca334

--- service/src/mainapp.cpp
+++ service/src/mainapp.cpp
@@ -166,8 +166,27 @@ static void logLastPanicEvent()
 static void logResiliencyErrorEvent(const uint8_t majorErrorCode,
                                     const uint8_t minorErrorCode)
 {
+    uint8_t cpldRoTRev = 0;
+    if (0 != readCpldReg(ActionType::readRoT, cpldRoTRev))
+    {
+        return;
+    }
+
     auto it = majorErrorCodeMap.find(majorErrorCode);
-    if (it == majorErrorCodeMap.end())
+    if (cpldRoTRev == 0x02)
+    {
+        it = majorErrorCodeMapNew.find(majorErrorCode);
+        if (it == majorErrorCodeMapNew.end())
+        {
+            it = majorErrorCodeMap.find(majorErrorCode);
+            if (it == majorErrorCodeMap.end())
+            {
+                // No matching found. So just return without logging event.
+                return;
+            }
+        }
+    }
+    else if (it == majorErrorCodeMap.end())
     {
         // No matching found. So just return without logging event.
         return;
